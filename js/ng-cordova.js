1849557350d760eea75ce4c41eb66907a9e9c803
Santiago Pintos <santiagopintos2@gmail.com>
2014-11-23 12:00:26 -0500

    commit www

---
+++ js/ng-cordova.js
@@ -0,0 +1,152 @@
+/*!
+ * ngCordova
+ * Copyright 2014 Drifty Co. http://drifty.com/
+ * See LICENSE in this repository for license information
+ */
+(function(){
+
+angular.module('ngCordova', [
+  'ngCordova.plugins'
+]);
+
+// insert modules here
+
+// add to this list
+angular.module('ngCordova.plugins', [
+  'ngCordova.plugins.sqlite' ,
+  'ngCordova.plugins.toast'
+]);
+
+angular.module('ngCordova.plugins.sqlite', [])
+
+  .factory('$cordovaSQLite', ['$q', function ($q) {
+
+    return  {
+      openDB: function (dbName) {
+        return  window.sqlitePlugin.openDatabase({name: dbName});
+      },
+
+
+      openDBBackground: function (dbName) {
+        return window.sqlitePlugin.openDatabase({name: dbName, bgType: 1});
+      },
+
+      execute: function (db, query, binding) {
+        q = $q.defer();
+        db.transaction(function (tx) {
+          tx.executeSql(query, binding, function (tx, result) {
+              q.resolve(result);
+            },
+            function (transaction, error) {
+              q.reject(error);
+            });
+        });
+        return q.promise;
+      },
+
+      nestedExecute: function (db, query1, query2, binding1, binding2) {
+        q = $q.defer();
+
+        db.transaction(function (tx) {
+            tx.executeSql(query1, binding1, function (tx, result) {
+              q.resolve(result);
+              tx.executeSql(query2, binding2, function (tx, res) {
+                q.resolve(res);
+              })
+            })
+          },
+          function (transaction, error) {
+            q.reject(error);
+          });
+
+        return q.promise;
+      }
+
+      // more methods here
+    }
+  }]);
+
+  // install   :      cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
+// link      :      https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin
+
+angular.module('ngCordova.plugins.toast', [])
+
+  .factory('$cordovaToast', ['$q', '$window', function ($q, $window) {
+
+    return {
+      showShortTop: function (message) {
+        var q = $q.defer();
+        $window.plugins.toast.showShortTop(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error);
+        });
+        return q.promise;
+      },
+
+      showShortCenter: function (message) {
+        var q = $q.defer();
+        $window.plugins.toast.showShortCenter(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error);
+        });
+        return q.promise;
+      },
+
+      showShortBottom: function (message) {
+        var q = $q.defer();
+        $window.plugins.toast.showShortBottom(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error);
+        });
+        return q.promise;
+      },
+
+      showLongTop: function (message) {
+        var q = $q.defer();
+        $window.plugins.toast.showLongTop(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error);
+        });
+        return q.promise;
+      },
+
+      showLongCenter: function (message) {
+        var q = $q.defer();
+        $window.plugins.toast.showLongCenter(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error);
+        });
+        return q.promise;
+      },
+
+      showLongBottom: function (message) {
+        var q = $q.defer();
+        $window.plugins.toast.showLongBottom(message, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error);
+        });
+        return q.promise;
+      },
+
+
+      show: function (message, duration, position) {
+        var q = $q.defer();
+        $window.plugins.toast.show(message, duration, position, function (response) {
+          q.resolve(response);
+        }, function (error) {
+          q.reject(error);
+        });
+        return q.promise;
+      }
+    };
+
+  }]);
+  
+  
+})();
